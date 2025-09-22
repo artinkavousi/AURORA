ecd6164dccba2fdb06641e3af8815fd45f23b283
artinkavousi <68490964+artinkavousi@users.noreply.github.com>
2025-09-22 18:48:35 -0400

    fix(tweakpane): import EssentialsPlugin as named export for v4

--- src/conf.js
+++ src/conf.js
@@ -1,5 +1,5 @@
 import {Pane} from 'tweakpane';
-import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
+import { EssentialsPlugin } from '@tweakpane/plugin-essentials';
 import mobile from "is-mobile";
 import * as THREE from "three/webgpu";
 

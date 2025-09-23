66584cceacf1495581625300e02197eaacb3eb16
artinkavousi <68490964+artinkavousi@users.noreply.github.com>
2025-09-23 00:09:04 -0400

    fix(tweakpane): use default import for @tweakpane/plugin-essentials to match build output

--- src/ui/audioPanel.js
+++ src/ui/audioPanel.js
@@ -1,5 +1,5 @@
 import { Pane } from 'tweakpane';
-import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
+import EssentialsPlugin from '@tweakpane/plugin-essentials';
 
 export default class AudioPanel {
   constructor(engine, conf, router) {
